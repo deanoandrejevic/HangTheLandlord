ab5a95859b0727f7b6f0b99ce5518c93208fd4e1
deanoandrejevic <deanoandrejevic@gmail.com>
2021-06-20 12:43:13 +0000

    started adding JS, questions and displaying questions

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,8 +13,49 @@ function showRules(event) {
   }
 }
 
+// Varibles to be used throughout
+
+let userScore = 0;
+let currentQuestions = {};
+let questionCounter = 0;
+let levelQuestions = [];
+
+
+// Questions for the quiz
+// Level 1 Questions
+
+var levelOneQuestions = [
+  {
+    question:
+      "What former disney channel star recently released the song Good 4 U?",
+    a: "Olivia Rodiguiz",
+    b: "Selena Gomez",
+    c: "Demi Lavato",
+    d: "Vanessa Hudgens",
+    correctAnswer: "a",
+  },
+  {
+    question:
+      "What red haired sensation has announced he will be a sponsor for Ipswich town football club in the 21/22 season?",
+    a: "Hayley Williams",
+    b: "Florence Welsh",
+    c: "Ed Sheeran",
+    d: "Elly Jackson",
+    correctAnswer: "c",
+  },
+];
+
+// Level 2 Questions
+
+// Level 3 Questions
+
+// Level 4 Questions
+
+// Select Random Question and display
+
+
+// User score
+
 // function for starting the game
 
-function startGame () {
-  
-}
+function startGame() {}
