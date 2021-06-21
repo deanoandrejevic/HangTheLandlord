69746e2d95ef9e840819084d40b8df264af1000c
deanoandrejevic <deanoandrejevic@gmail.com>
2021-06-21 16:33:37 +0000

    questions and answers are now being displayed, and difficulty scales

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,7 @@
 // retriving parts of the quiz layout to be called on later
 
-const questionsBox = document.getElementById("question-box");
-const answerBox = document.getElementsByClassName("answer-box");
+const questionsText = document.getElementById("question-text");
+const answerText = Array.from(document.getElementsByClassName("answer-text"));
 
 // function to toggle between showing rules and hiding rules
 
@@ -21,107 +21,159 @@ function showRules(event) {
 // Varibles to be used throughout
 
 let userScore = 0;
-let currentQuestion1 = {};
-let currentQuestion2 = {};
-let currentQuestion3 = {};
-let currentQuestion4 = {};
+let currentQuestionOne = {};
+let currentQuestionTwo = {};
+let currentQuestionThree = {};
+let currentQuestionFour = {};
 let questionNumber = 0;
-let levelQuestions = [];
+let avalibleQuestions = [];
 
 // Questions for the quiz
 // Level 1 Questions
-
-var levelOneQuestions = [
+var questionsLvl1 = [
   {
     question:
       "What former disney channel star recently released the song Good 4 U?",
-    a: "Olivia Rodiguiz",
-    b: "Selena Gomez",
-    c: "Demi Lavato",
-    d: "Vanessa Hudgens",
-    correctAnswer: "a",
+    choiceA: "Olivia Rodiguiz",
+    choiceB: "Selena Gomez",
+    choiceC: "Demi Lovato",
+    choiceD: "Vanessa Hudgens",
+    correctAnswer: "choiceA",
   },
   {
     question:
       "What red haired sensation has announced he will be a sponsor for Ipswich town football club in the 21/22 season?",
-    a: "Hayley Williams",
-    b: "Florence Welsh",
-    c: "Ed Sheeran",
-    d: "Elly Jackson",
-    correctAnswer: "c",
+    choiceA: "Hayley Williams",
+    choiceB: "Florence Welsh",
+    choiceC: "Ed Sheeran",
+    choiceD: "Elly Jackson",
+    correctAnswer: "choiceC",
+  },
+  {
+    question:
+      "In 2020 Dua Lipa released her second studio album, what is the name of that album?",
+    choiceA: "Past Nostalgia",
+    choiceB: "Future Nostalgia",
+    choiceC: "Present Nostalgia",
+    choiceD: "Nostalgic Past",
+    correctAnswer: "choiceB",
+  },
+  {
+    question:
+      "Harry Styles took the music and TikTok world by storm with the song Watermelon….",
+    choiceA: "Fruit",
+    choiceB: "Juice",
+    choiceC: "Honey",
+    choiceD: "Sugar",
+    correctAnswer: "choiceD",
+  },
+  {
+    question:
+      "On 31st October 1975, british rock group Queen released what is regarded as one of the best songs ever created but in 2018 what biographical film took on the same name?",
+    choiceA: "Radio Ga Ga",
+    choiceB: "The Show Must Go On",
+    choiceC: "Boheimem Rhapsody",
+    choiceD: "We Will Rock You",
+    correctAnswer: "choiceC",
   },
 ];
-
 // Level 2 Questions
-
-var levelTwoQuestions = [
+var questionsLvl2 = [
   {
     question:
       "The song Free Bird was performed by which Florida based music group",
-    a: "KC and The Sunshine Band",
-    b: "The Allman Brothers",
-    c: "Lynyrd Skynyrd",
-    d: "Tom Petty and the Heartbreakers",
-    correctAnswer: "c",
+    choiceA: "KC and The Sunshine Band",
+    choiceB: "The Allman Brothers",
+    choiceC: "Lynyrd Skynyrd",
+    choiceD: "Tom Petty and the Heartbreakers",
+    correctAnswer: "choiceC",
   },
 ];
-
 // Level 3 Questions
-
-var levelThreeQuestions = [
+var questionsLvl3 = [
   {
     question:
       "The song Land of Confusion was on the Genesis record 'Invisible Touch', but which metal group covered the song in 2005",
-    a: "Avenged Sevenfold",
-    b: "Drowning Pool",
-    c: "Slipknot",
-    d: "Disturbed",
-    correctAnswer: "d",
+    choiceA: "Avenged Sevenfold",
+    choiceB: "Drowning Pool",
+    choiceC: "Slipknot",
+    choiceD: "Disturbed",
+    correctAnswer: "choiceD",
   },
 ];
-
 // Level 4 Questions
-
-var levelFourQuestions = [
+var questionsLvl4 = [
   {
     question:
       "The song 'I Don't Want To Set The World On Fire' was written in 1938, but which artists rendition of the song has become synonmus with the popular gaming franchise, Fallout",
-    a: "Vera Lynn",
-    b: "Harlan Leonard",
-    c: "Horace Hiedt",
-    d: "The Ink Spots",
-    correctAnswer: "d",
+    choiceA: "Vera Lynn",
+    choiceB: "Harlan Leonard",
+    choiceC: "Horace Hiedt",
+    choiceD: "The Ink Spots",
+    correctAnswer: "choiceD",
   },
 ];
 
 // Select Random Question and display
 
 function displayQuestion() {
-  const randomLevelOne = Math.floor(Math.random() * levelOneQuestions.length);
-  const randomLevelTwo = Math.floor(Math.random() * levelTwoQuestions.length);
-  const randomLevelThree = Math.floor(Math.random() * levelThreeQuestions.length);
-  const randomLevelFour = Math.floor(Math.random() * levelFourQuestions.length);
+  const lvlOne = Math.floor(Math.random() * questionsLvl1.length);
+  const lvlTwo = Math.floor(Math.random() * questionsLvl2.length);
+  const lvlThree = Math.floor(Math.random() * questionsLvl3.length);
+  const lvlFour = Math.floor(Math.random() * questionsLvl4.length);
 
-  currentQuestion1 = levelOneQuestions[randomLevelOne];
-  currentQuestion2 = levelTwoQuestions[randomLevelTwo];
-  currentQuestion3 = levelThreeQuestions[randomLevelThree];
-  currentQuestion4 = levelFourQuestions[randomLevelFour];
+  currentQuestionOne = avalibleQuestions[lvlOne];
+  currentQuestionTwo = avalibleQuestions[lvlTwo];
+  currentQuestionThree = avalibleQuestions[lvlThree];
+  currentQuestionFour = avalibleQuestions[lvlFour];
 
   if (userScore <= 5) {
-    questionsBox.innerHTML = currentQuestion1.question;
+    questionsText.innerText = currentQuestionOne.question;
   } else if (userScore <= 10) {
-    questionsBox.innerHTML = currentQuestion2.question;
+    questionsText.innerText = currentQuestionTwo.question;
   } else if (userScore <= 15) {
-    questionsBox.innerHTML = currentQuestion3.question;
+    questionsText.innerText = currentQuestionThree.question;
   } else if (userScore <= 20) {
-    questionsBox.innerHTML = currentQuestion4.question;
+    questionsText.innerText = currentQuestionFour.question;
   }
+
+  answerText.forEach((choice) => {
+    var anwserChoice = choice.dataset["letter"];
+    if (userScore <= 5) {
+      choice.innerText = currentQuestionOne["choice" + anwserChoice];
+    } else if (userScore <= 10) {
+      choice.innerText = currentQuestionTwo["choice" + anwserChoice];
+    } else if (userScore <= 15) {
+      choice.innerText = currentQuestionThree["choice" + anwserChoice];
+    } else if (userScore <= 20) {
+      choice.innerText = currentQuestionFour["choice" + anwserChoice];
+    }
+  });
 }
 
-// User score
+// function increaseDifficulty() {
+//   if (userScore <= 5) {
+//     questionsText.innerText = currentQuestionOne.question;
+//   } else if (userScore <= 10) {
+//     questionsText.innerText = currentQuestionTwo.question;
+//   } else if (userScore <= 15) {
+//     questionsText.innerText = currentQuestionThree.question;
+//   } else if (userScore <= 20) {
+//     questionsText.innerText = currentQuestionFour.question;
+//   }
+// }
 
-
+// Check answer and increment score
 
 // function for starting the game
 
-function startGame() {}
+function startGame() {
+  (avalibleQuestions = [...questionsLvl1]),
+    [...questionsLvl2],
+    [...questionsLvl3],
+    [...questionsLvl4];
+  userScore = 1;
+  displayQuestion();
+}
+
+startGame();
